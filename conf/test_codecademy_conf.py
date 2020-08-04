031d3baf8a039094b0e83d704716d8f90370e929
Drishya <drishya.tm@qxf2.com>
2020-08-04 18:01:50 +0530

    all references added here along with all urls

--- conf/test_codecademy_conf.py
+++ conf/test_codecademy_conf.py
@@ -1,18 +1,26 @@
 
-
-#Conf  for home_page
-
+#conf for login page 
+login_url = 'login'
 redirect_title_home = "Dashboard"
 
+#conf  for home page
+home_url = 'learn'
 search_text_course = "Java"
 course_name_popular= "HTML"
 redirect_title_catalog = "Catalog Home | Codecademy"
 redirect_title_search = "Search"
 
-
+#conf for catalog page
+catalog_url = 'catalog'
 course_name = "SQL"
 redirect_title_course = "SQL Courses & Tutorials"
 
-"This is conf file for the course page"
+#conf file for the course page"
+course_url = 'catalog/language/sql'
 course_heading = 'SQL Courses & Tutorials | Codecademy'
+recommended_course = 'curriculum-card-learn-sql'
 redirect_title_course_recommended = "Learn SQL"
+
+#conf file for enroll course page 
+enroll_url = 'learn/learn-sql'
+enroll_heading = 'Learn SQL | Codecademy'
