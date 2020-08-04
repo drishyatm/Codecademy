f46f3380233e84d80f4d8b430178c792dcdf87ea
Drishya <drishya.tm@qxf2.com>
2020-08-04 18:10:31 +0530

    added url as reference

--- page_objects/codecademy_login_page.py
+++ page_objects/codecademy_login_page.py
@@ -4,12 +4,13 @@ URL: login
 """
 from .Base_Page import Base_Page
 from .codecademy_login_page_object import Codecademy_Login_Page_Object
+import conf.test_codecademy_conf as conf
 
 class Codecademy_Login_Page(Base_Page, Codecademy_Login_Page_Object):
     "Page Object for the Codecademy Login page"
 
     def start(self):
         "Use this method to go to specific URL -- if needed"
-        url = 'login'
+        url = conf.login_url
         print("launching the Login page")
         self.open(url)
