1ed83acf45d5d36a370637ebabf16d7c02b60046
Drishya <drishya.tm@qxf2.com>
2020-08-04 11:49:27 +0530

    removed the other conf file and encapuslated to one

--- conf/course_page_conf.py
+++
@@ -1,3 +0,0 @@
-"This is conf file for the course page"
-course_heading = 'SQL Courses & Tutorials | Codecademy'
-redirect_title_course = "Learn SQL"
--- conf/home_page_conf.py
+++
@@ -1,8 +0,0 @@
-"""
-Conf file for home_page
-"""
-
-search_text_course = "Java"
-course_name = "HTML"
-redirect_title_catalog = "Catalog Home | Codecademy"
-redirect_title_search = "Search"
